354c2711bbfc6a0c3143868eada8f8ca3c436e33
Olga Bazhenova <olgabazhenova07@yandex.ru>
2019-06-29 12:39:27 +0300

    Переводит video.js в прототипный стиль

--- source/js/video.js
+++ source/js/video.js
@@ -3,48 +3,22 @@
 (function () {
   function findVideos() {
     var videos = document.querySelectorAll('.video');
+    var youTubePlayer = new YouTubePlayer();
 
     for (var i = 0; i < videos.length; i++) {
-      setupVideo(videos[i]);
+      youTubePlayer.setup(videos[i]);
     }
   }
 
-  function setupVideo(video) {
-    var link = video.querySelector('.video__link');
-    var media = video.querySelector('.video__media');
-    var button = video.querySelector('.video__button');
-    var id = parseLinkURL(link);
+  function YouTubePlayer() {}
 
-    function onVideoClick() {
-      var iframe = createIframe(id);
-
-      link.parentElement.removeChild(link);
-      button.parentElement.removeChild(button);
-      video.appendChild(iframe);
-      video.removeEventListener('click', onVideoClick);
-    }
-
-    video.addEventListener('click', onVideoClick);
-    link.removeAttribute('href');
-    video.classList.add('video--enabled');
-  }
-
-  function parseLinkURL(link) {
-    var regexp = /youtu\.be\/([a-zA-Z0-9_-]+)/i;
-    var url = link.href;
-    var match = url.match(regexp);
-
-    return match[1];
-  }
-
-  function generateURL(id) {
-    var query = '?rel=0&showinfo=0&autoplay=1';
-
-    return 'https://www.youtube.com/embed/' + id + query;
-  }
-
-  function createIframe(id) {
+  YouTubePlayer.prototype.createIframe = function (id) {
     var iframe = document.createElement('iframe');
+
+    function generateURL(id) {
+      var QUERY = '?rel=0&showinfo=0&autoplay=1';
+      return 'https://www.youtube.com/embed/' + id + QUERY;
+    }
 
     iframe.setAttribute('allowfullscreen', '');
     iframe.setAttribute('allow', 'autoplay');
@@ -52,7 +26,35 @@
     iframe.classList.add('video__media');
 
     return iframe;
-  }
+  };
+
+  YouTubePlayer.prototype.parseLinkURL = function (link) {
+    var regexp = /youtu\.be\/([a-zA-Z0-9_-]+)/i;
+    var url = link.href;
+    var match = url.match(regexp);
+
+    return match[1];
+  };
+
+  YouTubePlayer.prototype.setup = function (video) {
+    var link = video.querySelector('.video__link');
+    var button = video.querySelector('.video__button');
+    var id = this.parseLinkURL(link);
+    var handler = onVideoClick.bind(this);
+
+    function onVideoClick() {
+      var iframe = this.createIframe(id);
+
+      link.parentElement.removeChild(link);
+      button.parentElement.removeChild(button);
+      video.appendChild(iframe);
+      video.removeEventListener('click', handler);
+    }
+
+    video.addEventListener('click', handler);
+    link.removeAttribute('href');
+    video.classList.add('video--enabled');
+  };
 
   findVideos();
 })();
